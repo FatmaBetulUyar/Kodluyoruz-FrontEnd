6ded5d059b940b34ac8ebb68e0ed65ba9ce9f3aa
betul <fbluyar2@gmail.com>
2021-09-06 15:25:30 +0300

    react temel

---
+++ REACT/todo-app-master/src/components/TodoHeader/TodoHeader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Title(props) {
+    return (
+        <h1>To Do LIST</h1>
+    );
+}
+
+export default Title;
---
+++ REACT/todo-app-master/src/components/TodoItem/TodoItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./TodoItem.css";
+
+
+function Todo(props) {
+  const { content } = props;
+  return <div >{content}<button>Sil</button></div>;
+}
+
+export default Todo;
